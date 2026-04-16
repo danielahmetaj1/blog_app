6af50032a3e171edd63bdada75a115f9ab0b7d03
denisdaja <102697632+denisdaja@users.noreply.github.com>
2026-04-17 00:41:30 +0200

    stilizimi dashboard (jo i perfunduar)

--- main.js
+++ main.js
@@ -18,3 +18,18 @@ const closeNav= () => {
 }
 openNavBtn.addEventListener('click', openNav);
 closeNavBtn.addEventListener('click', closeNav);
+
+
+
+
+const sidebar = document.querySelector('aside');
+const showSidebarBtn = document.querySelector('#show__sidebar-btn');
+const hideSidebarBtn = document.querySelector('#hide__sidebar-btn');
+//hapja e sidebar
+
+const showSideBar = () => { 
+    sidebar.style.left = '0';
+    showSidebarBtn.style.display = 'none';
+    hideSidebarBtn.style.display = 'inline-block';
+}
+showSidebarBtn.addEventListener('click',showSideBar );
