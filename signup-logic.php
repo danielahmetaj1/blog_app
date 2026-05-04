a9571cc7086fb6a0fa531af79e008fec959c914e
danielahmetaj1 <danielahmetaj22@gmail.com>
2026-05-04 23:16:54 +0200

    rregulim i query ne sign up

--- signup-logic.php
+++ signup-logic.php
@@ -42,7 +42,7 @@ require 'config/database.php';
             $hashed_password = password_hash($createpassword, PASSWORD_DEFAULT);
              
             //kontrolloni nese emri i perdoruesit ose emaili ekziston ne bazin e te dhenave
-            $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email";
+            $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' ";
             $user_check_result = mysqli_query($connection, $user_check_query);
             if(mysqli_num_rows($user_check_result) > 0){
                 $_SESSION['signup'] = "Username or Email already exists";
