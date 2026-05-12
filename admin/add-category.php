5393dc8e2042aa3f7a25ad13fadb81a1d46c3f85
denisdaja <102697632+denisdaja@users.noreply.github.com>
2026-05-12 16:44:59 +0200

    regullim kohe

--- admin/add-category.php
+++ admin/add-category.php
@@ -20,7 +20,7 @@ unset($_SESSION['add-category-data']);
         <form action="<?= ROOT_URL ?>admin/add-category-logic.php" method="POST">
             <input type="text" name="title" value="<?= $title ?>" placeholder="Titulli">
             <textarea rows="4" name="slug" placeholder="Pershkrimi"><?= $slug ?></textarea>
-            <textarea name="created_at" placeholder="Data e Krijimit"><?= $created_at ?></textarea>
+            <textarea name="created_at" placeholder="Data e Krijimit(yyyy-mm-dd)"><?= $created_at ?></textarea>
             <button type="submit" name="submit" class="btn">Shto Kategori</button>
         </form>
 
