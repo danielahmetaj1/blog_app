f7adeb3f0959fd2927a22bf9112ca555e28a7157
denisdaja <102697632+denisdaja@users.noreply.github.com>
2026-05-14 17:52:09 +0200

    fetch posts

--- admin/add-post.php
+++ admin/add-post.php
@@ -4,23 +4,36 @@ include 'partials/header.php';
 //mer kategorite nga databaza
 $query = "SELECT * FROM categories";
 $categories = mysqli_query($connection, $query);
+
+//kthe mbrapsh te dhenat e formes nese form ka qene invalid
+$title = $_SESSION['add-post-data']['title'] ?? null;
+$body = $_SESSION['add-post-data']['body'] ?? null; 
+unset($_SESSION['add-post-data']);
+
 ?>
         
 <section class="form__section" >
     <div class="container form__section-container">
         <h2>Shto Post</h2>
+        <?php if(isset($_SESSION['add-post'])): ?>
         <div class="alert__message error">
-            <p>Ky eshte nje mesazh gabimi</p>
+            <p>
+                <?= $_SESSION['add-post']; 
+                unset($_SESSION['add-post']);
+                ?>
+
+            </p>
         </div>
+        <?php endif ?>
         <form action="<?= ROOT_URL  ?>admin/add-post-logic.php" enctype="multipart/form-data" method="POST">
-            <input type="text" name="title" placeholder="Titulli">
+            <input type="text" name="title" value="<?= $title ?>" placeholder="Titulli">
             <select name="category_id">
                 <?php while($category_id = mysqli_fetch_assoc($categories)): ?>
                 <option value="<?= $category_id['id'] ?>"><?= $category_id['title'] ?></option>
                <?php endwhile ?>
-
+        
             </select>
-            <textarea rows="10" name="body" placeholder="Zhvillimi"></textarea>
+            <textarea rows="10" name="body" placeholder="Zhvillimi" ><?= $body ?></textarea>
             <?php if(isset($_SESSION['user_is_admin'])): ?>
             <div class="form__control inline">
                 <input type="checkbox" id="is_featured" value="1" name="is_featured" checked>
--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,24 @@
 <?php
 include 'partials/header.php';
+
+//bejme fetch postet e userit aktual
+$current_user_id = $_SESSION['user_id'];
+$querry= "SELECT id, title, category_id FROM posts WHERE user_id=$current_user_id ORDER BY id DESC";
+$posts = mysqli_query($connection, $querry);
 ?>
 
 
 
 <section class="dashboard">
+    <?php if(isset($_SESSION['add-post-success'])): ?> 
+                <div class="alert__message success container">
+                    <p>
+                        <?= $_SESSION['add-post-success'];
+                        unset($_SESSION['add-post-success']);
+                        ?>
+                    </p>
+                </div>
+    <?php endif ?>
     <div class="container dashboard__container">
         <button id="show__sidebar-btn" class="sidebar__toggle"><i class="uil uil-angle-right-b"></i></button>
         <button id="hide__sidebar-btn" class="sidebar__toggle"><i class="uil uil-angle-left-b"></i></button>
@@ -45,7 +59,8 @@ include 'partials/header.php';
             </ul>
         </aside>
         <main>
-            <h2>Manage Users</h2>
+            <h2>Manage Posts</h2>
+            <?php if(mysqli_num_rows($posts) > 0): ?>
             <table>
                 <thead>
                     <tr>
@@ -56,31 +71,28 @@ include 'partials/header.php';
                     </tr>
                 </thead>
                 <tbody>
+                    <?php while($post = mysqli_fetch_assoc($posts)) : ?>
+                        <?php //bejme fetch te kategorise se postit
+                        $category_id = $post['category_id'];
+                        $category_querry = "SELECT title FROM categories WHERE id=$category_id";
+                        $category_result = mysqli_query($connection, $category_querry);
+                        $category = mysqli_fetch_assoc($category_result);
+                            ?>
                     <tr>
-                        <td>Lorem ipsum</td>
-                        <td>Wild Life</td>
-                        <td><a href="edit-post.php" class="btn sm">Edit</a></td>
-                        <td><a href="delete-category.php" class="btn sm danger">Fshi</a></td>
+                        <td><?= $post['title'] ?> </td>
+                        <td><?= $category['title'] ?></td>
+                        <td><a href="<?= ROOT_URL ?>admin/edit-post.php?id=<?= $post['id'] ?>" class="btn sm">Edit</a></td>
+                        <td><a href="<?= ROOT_URL ?>admin/delete-post.php?id=<?= $post['id'] ?>" class="btn sm danger">Fshi</a></td>
 
                     </tr>
-                    <tr>
-                        <td>Lorem ipsum</td>
-                        <td>Wild Life</td>
-                        <td><a href="edit-post.php" class="btn sm">Edit</a></td>
-                        <td><a href="delete-category.php" class="btn sm danger">Fshi</a></td>
-
-                    </tr>
-                    <tr>
-                        <td>Lorem ipsum</td>
-                        <td>Wild Life</td>
-                        <td><a href="edit-post.php" class="btn sm">Edit</a></td>
-                        <td><a href="delete-category.php" class="btn sm danger">Fshi</a></td>
-
-                    </tr>
-
+                    <?php endwhile ?>
 
                 </tbody>
             </table>
+            <?php else: ?>
+           <div class="alert__message error">  <?= "nuk u gjend asnje post" ?>
+           </div>
+           <?php endif ?>
         </main>
     </div>
 </section>
